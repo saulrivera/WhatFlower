77572f7ab4a38b2841056988b904d0bab535ba93
Saul Rivera <saulriveal@gmail.com>
2020-05-10 20:52:32 -0500

    Adding image recognition

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private var imagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .camera
+        imagePickerController.allowsEditing = true
     }
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    private func predict(from image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Error loading the model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Error loading the results")
+            }
+            
+            if let firstResult = results.first {
+                self.navigationItem.title = firstResult.identifier.capitalized
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+}
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imagePicked = info[.editedImage] as? UIImage {
+            imageView.image = imagePicked
+            
+            guard let ciimage = CIImage(image: imagePicked) else {
+                fatalError("Error converting to CIImage")
+            }
+            
+            predict(from: ciimage)
+        }
+        imagePickerController.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
